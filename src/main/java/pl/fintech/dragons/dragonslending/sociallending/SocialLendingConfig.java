57aa84f120b3e06c64858b061e332abf90fa7c40
Krystian Soloducha <krystsolo@gmail.com>
2020-11-20 15:59:18 +0100

    DSL-70 add socialLendingConfig containing all module configs

---
+++ src/main/java/pl/fintech/dragons/dragonslending/sociallending/SocialLendingConfig.java
@@ -0,0 +1,13 @@
+package pl.fintech.dragons.dragonslending.sociallending;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import pl.fintech.dragons.dragonslending.common.events.publisher.EventPublisherConfig;
+import pl.fintech.dragons.dragonslending.sociallending.identity.infrastructure.UserConfig;
+import pl.fintech.dragons.dragonslending.sociallending.payment.account.infrastructure.AccountConfig;
+import pl.fintech.dragons.dragonslending.sociallending.security.SecurityConfig;
+
+@Configuration
+@Import({SecurityConfig.class, AccountConfig.class, UserConfig.class, EventPublisherConfig.class})
+public class SocialLendingConfig {
+}
