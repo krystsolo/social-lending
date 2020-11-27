dd70bb1a896e1bf376d04f561dadd13d109e078e
Krystian Soloducha <krystsolo@gmail.com>
2020-11-28 00:01:46 +0100

    DSL-103 add handling offer selection event in account

--- src/main/java/pl/fintech/dragons/dragonslending/sociallending/payment/account/application/OfferEventHandler.java
+++ src/main/java/pl/fintech/dragons/dragonslending/sociallending/payment/account/application/OfferEventHandler.java
@@ -3,10 +3,13 @@ package pl.fintech.dragons.dragonslending.sociallending.payment.account.applicat
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
 import org.springframework.transaction.annotation.Transactional;
+import pl.fintech.dragons.dragonslending.sociallending.offer.OfferSelected;
 import pl.fintech.dragons.dragonslending.sociallending.offer.OfferSubmitted;
 import pl.fintech.dragons.dragonslending.sociallending.offer.OfferTerminated;
 import pl.fintech.dragons.dragonslending.sociallending.payment.account.domain.Account;
 import pl.fintech.dragons.dragonslending.sociallending.payment.account.domain.AccountRepository;
+
+import java.math.BigDecimal;
 
 @RequiredArgsConstructor
 @Transactional
@@ -24,5 +27,17 @@ public class OfferEventHandler {
     public void handleOfferTerminated(OfferTerminated event) {
         Account account = accountRepository.getOneByUserId(event.getUserId());
         account.unfreeze(event.getAmount());
+    }
+
+    @EventListener
+    public void handleOfferSelection(OfferSelected event) {
+        BigDecimal amount = event.getAmount();
+
+        Account lenderAccount = accountRepository.getOneByUserId(event.getLenderId());
+        lenderAccount.unfreeze(amount);
+        lenderAccount.withdraw(amount);
+
+        Account borrowerAccount = accountRepository.getOneByUserId(event.getBorrowerId());
+        borrowerAccount.deposit(amount);
     }
 }
--- src/test/groovy/pl/fintech/dragons/dragonslending/sociallending/payment/account/application/OfferEventHandlerTest.groovy
+++ src/test/groovy/pl/fintech/dragons/dragonslending/sociallending/payment/account/application/OfferEventHandlerTest.groovy
@@ -1,6 +1,6 @@
 package pl.fintech.dragons.dragonslending.sociallending.payment.account.application
 
-
+import pl.fintech.dragons.dragonslending.sociallending.offer.OfferSelected
 import pl.fintech.dragons.dragonslending.sociallending.offer.OfferSubmitted
 import pl.fintech.dragons.dragonslending.sociallending.offer.OfferTerminated
 import pl.fintech.dragons.dragonslending.sociallending.payment.account.domain.Account
@@ -43,5 +43,26 @@ class OfferEventHandlerTest extends Specification {
 
         then:
         1 * accountRepository.getOneByUserId(userId) >> account
+    }
+
+    def "should handle offer selection event"() {
+        given:
+        UUID borrowerId = UUID.randomUUID()
+        Account borrowerAccount = new Account(borrowerId)
+        accountRepository.getOneByUserId(borrowerId) >> borrowerAccount
+
+        UUID lenderId = UUID.randomUUID()
+        Account lenderAccount = new Account(lenderId)
+        lenderAccount.deposit(100 as BigDecimal)
+        lenderAccount.freeze(BigDecimal.TEN)
+        accountRepository.getOneByUserId(lenderId) >> lenderAccount
+
+        when:
+        offerEventHandler.handleOfferSelection(OfferSelected.now(
+                UUID.randomUUID(), UUID.randomUUID(), borrowerId, lenderId, BigDecimal.TEN, 2.0, 5))
+
+        then:
+        borrowerAccount.getBalance() == BigDecimal.TEN
+        lenderAccount.getBalance() == 90 as BigDecimal
     }
 }
